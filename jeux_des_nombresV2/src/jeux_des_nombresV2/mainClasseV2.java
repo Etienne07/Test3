a686101c4e5262648fb254296436528bbef706fb
Alexis <Alexis@10.60.176.6>
2019-03-13 23:38:40 +0100

    vgbhnj

--- jeux_des_nombresV2/src/jeux_des_nombresV2/mainClasseV2.java
+++ jeux_des_nombresV2/src/jeux_des_nombresV2/mainClasseV2.java
@@ -1,7 +1,7 @@
 package jeux_des_nombresV2;
 import java.util.Arrays;
 import java.util.Scanner;
-
+// debut class mainClassV2
 public class mainClasseV2 {
 	public static void main(String[] args)
 	{
